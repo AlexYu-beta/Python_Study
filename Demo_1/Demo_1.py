105b64f5856a0e308603170059bea3f053e686df
alex2097 <yym19960930@163.com>
2017-02-06 16:57:27 +0800

    commit ver 0.1 add Demo_1

---
+++ Demo_1/Demo_1.py
@@ -0,0 +1,12 @@
+'''
+Demo_1.py is the very first start of my Python Self-study demonstration
+a simple implementation of I/O operation
+from "Python: Visual Quick Start Guide"
+'''
+nme = raw_input("please enter your name: ")
+'''
+it seems that function input() is more strict in grammar
+in this case when entering a "string" like a name, you should add '' to make it a string
+but if we use the function raw_input() instead, we can get rid of such trouble
+'''
+print('Hello ' + nme.capitalize() + '!')
