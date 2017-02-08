37e5d5accd661940d08766951ee7a04430481df7
Alex Yu <yym1996@gmail.com>
2017-02-08 13:35:31 +0800

    commit ver 0.3 add Demo_3 //basic function defining and calling //basic doctest implementation

---
+++ Demo_3/Demo_3.py
@@ -0,0 +1,16 @@
+'''
+Demo_3 implements drawing simple shapes by using module "shape"
+uses simple function define
+implements simple doctest
+--just open the terminal, get to the father directory of this Demo,
+and python it adding parameter -V at the end of the command line
+then the test will begin
+'''
+import shape
+shape.rectangle(5,10)
+print()
+print()
+shape.square(1)
+print()
+print()
+shape.triangle(10)
---
+++ Demo_3/shape.py
@@ -0,0 +1,49 @@
+'''
+An example from the textbook "Python: Visual QuickStart Guide"
+A collection of functions for printing simple shapes
+'''
+
+CHAR = '*'
+
+def rectangle(height,width):
+    """
+    this function prints a rectangle
+    :param height: the height of the rectangle
+    :param width: the width of the rectangle
+    :return: None
+    >>> rectangle(5,1)
+    *
+    *
+    *
+    *
+    *
+    """
+    for row in range(height):
+        for column in range(width):
+            print(CHAR, end ='')
+        print()
+
+def square(side):
+    """
+    this function prints a square
+    :param side: the side of the square
+    :return: None
+    >>> square(1)
+    *
+    """
+    rectangle(side,side)
+
+def triangle(height):
+    """
+    print a specific-shaped triangle
+    :param height: the height of the triangle
+    :return: None
+    """
+    for row in range(height):
+        for column in range(1,row+2):
+            print(CHAR, end = '')
+        print()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose = True)
